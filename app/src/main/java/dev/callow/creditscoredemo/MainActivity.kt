fa24d5fdac67302f35d11c13f1e50fdbbf828e39
Charlie Callow <charlie@linux.com>
2025-08-30 16:19:41 +0100

    Display credit score in a donut view

--- app/src/main/java/dev/callow/creditscoredemo/MainActivity.kt
+++ app/src/main/java/dev/callow/creditscoredemo/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -18,7 +23,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import dev.callow.creditscoredemo.ui.CreditReportUiState
 import dev.callow.creditscoredemo.ui.CreditScoreViewModel
@@ -65,8 +79,11 @@ fun CreditScoreScreen(uiState: CreditReportUiState, modifier: Modifier = Modifie
             }
 
             is CreditReportUiState.Success -> {
-                Text(
-                    text = "Your credit score is: ${uiState.report.creditReportInfo.score}"
+                val creditInfo = uiState.report.creditReportInfo
+                DonutView(
+                    value = creditInfo.score,
+                    maxValue = creditInfo.maxScoreValue,
+                    label = "Your credit score is:"
                 )
             }
 
@@ -75,6 +92,70 @@ fun CreditScoreScreen(uiState: CreditReportUiState, modifier: Modifier = Modifie
                     text = "Error: ${uiState.message}"
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun DonutView(value: Int, maxValue: Int, strokeWidth: Dp = 20.dp, size: Dp = 250.dp, label: String = "") {
+    val angle = 360f * value / maxValue.toFloat()
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.requiredSize(size)) {
+        Canvas(modifier = Modifier.size(size)) {
+            // Background arc
+            drawArc(
+                color = Color.LightGray,
+                startAngle = 0f,
+                sweepAngle = 360f,
+                useCenter = false,
+                style = Stroke(width = strokeWidth.toPx())
+            )
+            // Foreground arc
+            drawArc(
+                color = Color.Blue,
+                startAngle = -90f, // Start from the top
+                sweepAngle = angle,
+                useCenter = false,
+                style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Column {
+            // Label
+            Text(
+                text = label,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = (size.value / 14).dp.value.toInt().sp // Adjust font size based on diameter
+            )
+            // Value
+            Text(
+                text = value.toString(),
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                fontWeight = FontWeight.Bold,
+                fontSize = (size.value / 4).dp.value.toInt().sp // Adjust font size based on diameter
+            )
+            // Max value
+            Text(
+                text = "out of $maxValue",
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = (size.value / 14).dp.value.toInt().sp // Adjust font size based on diameter
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DonutViewPreview() {
+    CreditScoreDemoTheme {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            DonutView(value = 327, maxValue = 700, label = "Your credit score is:")
         }
     }
 }
