31734bc9cdc0ac78a97a5ef72e63b85cab47603b
Charlie Callow <charlie@linux.com>
2025-08-30 17:20:13 +0100

    Tweak the appearance of donut view to more accurately match the reference design

--- app/src/main/java/dev/callow/creditscoredemo/MainActivity.kt
+++ app/src/main/java/dev/callow/creditscoredemo/MainActivity.kt
@@ -24,6 +24,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -98,14 +100,14 @@ fun CreditScoreScreen(uiState: CreditReportUiState, modifier: Modifier = Modifie
 }
 
 @Composable
-fun DonutView(value: Int, maxValue: Int, strokeWidth: Dp = 10.dp, size: Dp = 250.dp, label: String = "") {
+fun DonutView(value: Int, maxValue: Int, strokeWidth: Dp = 4.dp, size: Dp = 250.dp, label: String = "") {
     val angle = remember(value, maxValue) { 360f * value / maxValue.toFloat() }
     val scoreColor = determineScoreColor(value = value, maxValue = maxValue) // Determine colour based on score
 
     Box(contentAlignment = Alignment.Center, modifier = Modifier.requiredSize(size)) {
         Canvas(modifier = Modifier.size(size)) {
             val strokeWidthPx = strokeWidth.toPx()
-            // Background arc
+            // Outer ring
             drawArc(
                 color = Color.LightGray,
                 startAngle = 0f,
@@ -113,13 +115,16 @@ fun DonutView(value: Int, maxValue: Int, strokeWidth: Dp = 10.dp, size: Dp = 250
                 useCenter = false,
                 style = Stroke(width = strokeWidthPx)
             )
-            // Foreground arc
+            // Inner arc (representing the score)
+            val inset = strokeWidthPx * 2 // Define an inset for the inner ring to create a gap
             drawArc(
                 color = scoreColor,
                 startAngle = -90f, // Start from the top
                 sweepAngle = angle,
                 useCenter = false,
-                style = Stroke(width = strokeWidthPx, cap = StrokeCap.Round)
+                style = Stroke(width = strokeWidthPx, cap = StrokeCap.Round),
+                topLeft = Offset(inset, inset),
+                size = Size(this.size.width - 2 * inset, this.size.height - 2 * inset)
             )
         }
         Column {
